8dbfc7c92727a6f4316082f0ef8646f7f79ffd6f
Md. Mahmud Uz Zaman <zmahmud26@gmail.com>
2023-06-09 23:33:01 +0600

    classes data get

--- index.js
+++ index.js
@@ -34,12 +34,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const classCollection = client.db("muzSports").collection("classes")
+
+/////////////////////////// Classes ////////////////////
+
+app.get('/classes', async(req, res)=>{
+    const result = await classCollection.find().toArray()
+    res.send(result)
+})
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
