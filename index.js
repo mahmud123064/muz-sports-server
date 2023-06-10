92e0ee38668292790e7ed53cadc412b17742bfbf
Md. Mahmud Uz Zaman <zmahmud26@gmail.com>
2023-06-10 22:11:35 +0600

    selectedClasses created

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.send("MUZ Sports is running")
 })
 
@@ -23,42 +23,55 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const classCollection = client.db("muzSports").collection("classes")
+        const classCollection = client.db("muzSports").collection("classes")
+        const selectedClassCollection = client.db("muzSports").collection("selectedClasses")
 
-/////////////////////////// Classes ////////////////////
+        /////////////////////////// Classes ////////////////////
 
-app.get('/classes', async(req, res)=>{
-    const result = await classCollection.find().toArray()
-    res.send(result)
-})
+        app.get('/classes', async (req, res) => {
+            const result = await classCollection.find().toArray()
+            res.send(result)
+        })
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+
+        // selected Class collection
+
+        app.post('/selectedClasses', async(req, res)=> {
+            const item = req.body;
+            const result = await selectedClassCollection.insertOne(item)
+            res.send(result)
+        })
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 //////////////////////////////
 
-app.listen(port, ()=> {
-console.log(`muz sorts is running on ${port}`);
+app.listen(port, () => {
+    console.log(`muz sorts is running on ${port}`);
 })
 
