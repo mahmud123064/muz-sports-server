197bf400ab412ece449c18640f23d332b30a106d
Md. Mahmud Uz Zaman <zmahmud26@gmail.com>
2023-06-12 17:06:17 +0600

    Admin api done

--- index.js
+++ index.js
@@ -41,21 +41,35 @@ async function run() {
 
         //////////////////////Users API /////////////////////
 
-        app.get('/users', async(req, res)=> {
+        app.get('/users', async (req, res) => {
             const result = await usersCollection.find().toArray()
             res.send(result)
         })
 
-        app.post('/users', async(req, res)=>{
+        app.post('/users', async (req, res) => {
             const user = req.body;
             const query = { email: user.email }
             const existingUser = await usersCollection.findOne(query)
-            if(existingUser){
+            if (existingUser) {
                 return res.send({ message: " user already exists" })
             }
             const result = await usersCollection.insertOne(user);
             res.send(result)
         })
+
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+        
 
 
 
