55fc8323514292143dbb73bfe0369c47001d9c15
Md. Mahmud Uz Zaman <zmahmud26@gmail.com>
2023-06-12 17:31:23 +0600

    instractor api done

--- index.js
+++ index.js
@@ -70,8 +70,19 @@ async function run() {
             res.send(result)
         })
         
+        /////////////////// Instructor  //////////////////
 
-
+          app.patch('/users/instructor/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    role: 'instructor'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
 
         /////////////////////////// Classes ////////////////////
 
